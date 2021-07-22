e3f3179ea557576ec28590459e3cb9649570d7f3
noe <noe.reyes92@yahoo.com>
2021-07-22 06:40:25 -0500

    commit tree f3bc5240bcddddb77a569139cfbb2364b5f499ee

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newDriver = { ...driver };
+    newDriver[key] = value;
+    return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(newDriver, key, value) {
+    newDriver[key] = value;
+    return newDriver;
+  } 
+
+function deleteFromDriverByKey(driver, key) {
+    const newDriver = { ...driver };
+    delete newDriver[key];
+    return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
+}
